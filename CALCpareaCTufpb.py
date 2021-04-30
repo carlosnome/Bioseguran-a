71425083d97545833312158d4bffc028221075f9
carlosnome <83453600+carlosnome@users.noreply.github.com>
2021-04-30 09:55:30 -0300

    Update CALCpareaCTufpb.py

--- CALCpareaCTufpb.py
+++ CALCpareaCTufpb.py
@@ -1,1 +1,59 @@
-print("Hello, World!")
+area = 25
+
+# Press the green button in the gutter to run the script.
+if area <= 7:
+    verm = "atividades presenciais suspensas"
+    lara = 1
+    amar = 1
+    verd =1
+    print(verm, amar, lara, verd)
+elif area <= 15:
+    verm = "atividades presenciais suspensas"
+    amar = 2
+    lara = 4
+    verd = 6
+    print(verm, amar, lara, verd)
+elif area <= 25:
+    verm = "atividades presenciais suspensas"
+    amar = 3
+    lara = 7
+    verd = 11
+    print(verm, amar, lara, verd)
+elif area <= 35:
+    verm = "atividades presenciais suspensas"
+    amar = 4
+    lara = 9
+    verd = 15
+    print(verm, amar, lara, verd)
+elif area <= 45:
+    verm = "atividades presenciais suspensas"
+    amar = 6
+    lara = 12
+    verd = 20
+    print(verm, amar, lara, verd)
+elif area <= 55:
+    verm = "atividades presenciais suspensas"
+    amar = 7
+    lara = 14
+    verd = 24
+    print(verm, amar, lara, verd)
+elif area <= 70:
+    verm = "atividades presenciais suspensas"
+    amar = 8
+    lara = 18
+    verd = 31
+    print(verm, amar, lara, verd)
+elif area <= 80:
+    verm = "atividades presenciais suspensas"
+    amar = 10
+    lara = 21
+    verd = 35
+    print(verm, amar, lara, verd)
+elif area <= 100:
+    verm = "atividades presenciais suspensas"
+    amar = 13
+    lara = 26
+    verd = 44
+    print(verm, amar, lara, verd)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
